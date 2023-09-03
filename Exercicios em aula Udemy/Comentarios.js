8c76d32ea240c9c3df43e295b58bf72f2b572ee2
Leonardo Gabriel Gomes <leonardo1996gomes@gmail.com>
2023-09-03 15:50:51 -0300

    Update Comentarios.js

--- Exercicios em aula Udemy/Comentarios.js
+++ Exercicios em aula Udemy/Comentarios.js
@@ -1,11 +1,12 @@
-// console.log é usado para exibir algo na tela do computador!
+// console.log é usado para exibir algo no console do computador!
 console.log("Passo #1");
 console.log("Passo #2"); // mais um exemplo de console.log!
 console.log("Passo #3");
 /*
-* Esse é um
-* comentario
-* usando múltiplas
-* linhas!
+ * É importante implantar comentarós mais proximos dos código para que fique facil a explicação do código.
+ * Lembrando sempre que comentarios irelevantes devem ser evitados.
+ * De preferencia a comentarios sempres legiveis.
+ 
+
 */
 
