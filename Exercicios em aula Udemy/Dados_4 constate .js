07742bc5a1f6dd56b1f0ce24eb510fb2a135b070
Leonardo Gabriel Gomes <leonardo1996gomes@gmail.com>
2023-09-03 12:54:21 -0300

    Alteração de nomes
    adicionando a aula constante em js e alterando nome dos diretórios.

---
+++ Exercicios em aula Udemy/Dados_4 constate .js
@@ -0,0 +1,6 @@
+
+// para definir a varial como uma constate utilizamos o termo const.
+const a = 10;
+console.log(a);
+
+// quando temos um valor dentro do código o valor de variavel que nao será alterada recomenda-se o uso de uma constate em vez da let ou var.
