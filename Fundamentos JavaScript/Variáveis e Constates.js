6ef6e42c27aa15bec2b0272bb228d309bd5c6d03
Leonardo Gabriel Gomes <leonardo1996gomes@gmail.com>
2023-09-03 16:15:31 -0300

    Create Variáveis e Constates.js
    Tudo o que for anotação e novidade de variável e afins ficarão anotados aqui.

---
+++ Fundamentos JavaScript/Variáveis e Constates.js
@@ -0,0 +1,11 @@
+//Reforçando Variáveis e constantes.
+var a = 2; //aqui definimos a variável com a palavra reservada var.
+let b = 3; //aqui estamos utilizando o let como palavra reservada em 2015 foi implantado a fora mais moderna de criar a variável.
+//quando trabalhamos com var podemos redeclarar dentro de um mesmo lugar a variável duas vezer
+// sempre que puder usaremos o let para declarar variável
+const c = 5; // neste caso utilizamos a palavra reservada const para declarar constate como o proprio nome indica nao podemos alterar o valor da constante c = 5 neste caso.
+//Tudo que nao tiver a nescessidade de modificação dentro do nosso código utilizaremos a constante para a declaração.
+
+console.log(a,b);
+console.log(c);
+
