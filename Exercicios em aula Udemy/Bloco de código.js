d63cf3e84b238c91eb1728331aefaf21823bbf5d
Leonardo Gabriel Gomes <leonardo1996gomes@gmail.com>
2023-09-03 11:30:05 -0300

    Aulas udemy
    exemplos  feitos em aula para fixação do conteúdo da Udemy

---
+++ Exercicios em aula Udemy/Bloco de código.js
@@ -0,0 +1,10 @@
+{
+    console.log("Teste #1");//Aqui temos uma sentença de código!
+    console.log("Teste #2");
+    console.log("Teste #3");
+} // Isso define um bloco de código.
+
+{
+    console.log("Teste #4");
+    console.log("Teste #5");
+}
---
+++ Exercicios em aula Udemy/Dados_2 .js
@@ -0,0 +1,12 @@
+/*
+Definimos a variavel por ver ou let no código.
+Logo após definirmos o indentificador da variavel (lembrando sempre de definir identificadores claros e objetivos para a variavel.)
+*/
+//calculando desconto como exemplo de identifacadores da variaveis utilizando let.
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(preco * (1 - desconto));
+// exemplo a baixo de boa pratica de programação no identificador com letra maiuscula para cada nova palavra no indentificador.
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
