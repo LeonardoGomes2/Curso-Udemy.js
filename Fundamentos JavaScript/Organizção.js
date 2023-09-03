4d6c1b838ce7f20f88bffb9352ee781b0ed0c86c
Leonardo Gabriel Gomes <leonardo1996gomes@gmail.com>
2023-09-03 15:26:38 -0300

    Create Organizção.js
    Organização do javascript.

---
+++ Fundamentos JavaScript/Organizção.js
@@ -0,0 +1,11 @@
+//JavaScript é organizado em sentenças de código e bloco de códigos.
+//Sentença de código podem ocupar mais de uma linha podem terminar com ; como tbm nao nescessita terminar com ;
+//Bloco de código sao delimitados por chaves {} 
+//Podemos ter blocos de codígo dentro de outros blocos de códigos.
+
+console.log("Sentença de código.");
+{
+    {
+        console.log('Olá Mundo'); //pode-se incerir uma String em aspas simples tanto como em aspas duplas.
+    }
+}
