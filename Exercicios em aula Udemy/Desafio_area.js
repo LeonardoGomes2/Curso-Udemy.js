154ae969528dae5873bee89372ff365527f1298b
Leonardo Gabriel Gomes <leonardo1996gomes@gmail.com>
2023-09-03 13:10:39 -0300

    Create Desafio_area.js
    Introdução do Mayh.PI

---
+++ Exercicios em aula Udemy/Desafio_area.js
@@ -0,0 +1,16 @@
+// PI * raido * raio
+// Calcule a area de uma circunferência.
+
+let raio = 10;
+const PI = 3.14159265; // como o valor de pi nao muda ele é definido como constate.
+const area = PI * raio * raio; // calculo da area.
+
+console.log("O valor da área é : " + area + " Metros Quadrados.");
+
+//Poderiamos utilizar o calculo do proprio vscode para usar o valor de pi porem buscamos usar o basico como solução neste código.
+
+console.log(Math.PI);//aqui o Math.pi traz o valor de pi como proprio comando do vscode.
+
+
+
+
