fb022603458c93f8ec11166576d037a4e550c300
Leonardo Gabriel Gomes <leonardo1996gomes@gmail.com>
2023-09-03 12:43:19 -0300

    Create Dados_3 dados basico.js
    Dados básicos de js.

---
+++ Exercicios em aula Udemy/Dados_3 dados basico.js
@@ -0,0 +1,14 @@
+let idade = 31;
+console.log(typeof 31); // A função typeof traz o tipo de variavel neste caso number.
+console.log(typeof idade); // Aqui utilizamos a função chamando o identificador e nao o valor da variavel.
+
+let salario = 4578.88;
+console.log(typeof salario);
+// No caso acima notamos que na execusão do typeof o valor trazido tambem se trata de number o javascrip nao faz diferenciação de numeros inteiros e com casas decimais.
+
+let estaChovendo = true; // variavel boleana que aceita verdadeiro ou falso no caso true or false.
+console.log(typeof estaChovendo); //boolean.
+
+console.log(typeof "Teste"); //Aqui temos o String podendo ser defino no js co aspas duplas ou simples.
+
+
