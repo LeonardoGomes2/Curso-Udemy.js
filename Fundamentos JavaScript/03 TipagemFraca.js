fd53a521b3bbc4c61b418a43952262371e233bd9
Leonardo Gabriel Gomes <leonardo1996gomes@gmail.com>
2023-09-03 16:29:03 -0300

    Update 03 TipagemFraca.js
    Descrição de tipagem fraca

---
+++ Fundamentos JavaScript/03 TipagemFraca.js
@@ -0,0 +1,15 @@
+//JS é uma linguagem conhecida como linguagem de fraca tipagem ou seja ela nao nescessita de indentificadores nas variáveis, como por exemplo o java que quando utilizamos um numero inteiro temos que definilo pela palavra reservada int.
+
+
+let qualquer = "legal";
+console.log(qualquer);
+console.log(typeof qualquer);//Reforçando o comando de visualização de tipo de variável.
+
+qualquer = 4.989879;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//Evite a escolha de nomes genéricos para as variáves e siglas pois sigras serão compreendidas por certas pessoas mais nao ha todas.
+//exemplo de sigla.
+let pqp = false //pqp produto quimico perigos ... 
+// a cima temos um exemplo de sigla com aplo sentido.
